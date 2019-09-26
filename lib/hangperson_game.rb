236e4aea36b7b37a0dc12af4696340285733b66e
Albert Huang <albert168@berkeley.edu>
2019-09-26 02:34:38 -0700

    part 1

--- lib/hangperson_game.rb
+++ lib/hangperson_game.rb
@@ -1,4 +1,5 @@
 class HangpersonGame
+  attr_accessor :word, :guesses, :wrong_guesses, :count
 
   # add the necessary class methods, attributes, etc. here
   # to make the tests in spec/hangperson_game_spec.rb pass.
@@ -10,6 +11,50 @@ class HangpersonGame
   
   def initialize(word)
     @word = word
+    @guesses = ''
+    @wrong_guesses = ''
+    @count = 0
+  end
+
+  def check_win_or_lose()    
+    word = word_with_guesses  
+    if(!(word.include? '-'))
+      return :win
+    end
+    if(@count >= 7)
+      return :lose
+    end
+    return :play
+  end
+
+  def guess(word)
+    if((word == nil) || (word == ''))
+      raise(ArgumentError)
+    elsif(!(word.alpha?))
+      raise(ArgumentError)
+    end
+    word.downcase!
+    if((@guesses.include? word) || (@wrong_guesses.include? word))
+      return false
+    end
+    if(!(@word.include? word))
+      @count += 1
+      @wrong_guesses.concat(word)
+    else
+      @guesses.concat(word)
+    end
+  end
+
+  def word_with_guesses()
+    temp = ''
+    @word.each_char { |char|
+      if(@guesses.include? char)
+        temp.concat(char)
+      else
+        temp.concat('-')
+      end
+    }
+    return temp
   end
 
   # You can test it by running $ bundle exec irb -I. -r app.rb
@@ -24,4 +69,10 @@ class HangpersonGame
     }
   end
 
+end
+
+class String
+  def alpha?
+    !!match(/^[[:alnum:]]+$/)
+  end
 end
